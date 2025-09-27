65ce3aa5aec0b8ea3b2f8e024d44fba8ca6749f2
Anosh Kudikala <102867566+ennu897@users.noreply.github.com>
2025-09-27 17:33:30 -0500

    Completed Rock-Paper-Scissors project

--- project-rock-paper-scissors/javaScript.js
+++ project-rock-paper-scissors/javaScript.js
@@ -1,64 +1,101 @@
+// https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
+
+
+// Step 1: Setup the project structure
+// console.log("Hello World!");
+
+
+// Step 2: Write the logic to get the computer choice
 function getComputerChoice(){
-    let random = Math.random();
-    if(random < 0.33){
+    let randonNum = Math.random();
+    if(randonNum < 0.33){
         return "rock";
-    }else if(random < 0.66){
+    }else if(randonNum < 0.66){
         return "paper";
     }else {
         return "scissors";
     }
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
+
+// Step 3: Write the logic to get the human choice
 function getHumanChoice(){
-    let userInput = prompt("your input should be rock or paper or scissors: ");
+    let userInput= prompt("Input either one of the following rock/paper/scissors : ");
     return userInput.toLowerCase();
 }
+// console.log(getHumanChoice());
 
+
+// Step 4: Declare the players score variables
 let humanScore = 0;
 let computerScore = 0;
 
+
+// Step 5: Write the logic to play a single round
 function playRound(humanChoice, computerChoice){
     if(humanChoice === computerChoice){
-        return "You got Tied!!!";
-    }else if (
-        (humanChoice === "rock" & computerChoice === "scissors") ||
-        (humanChoice === "paper" & computerChoice === "rock") ||
-        (humanChoice === "scissors" & computerChoice === "paper")
-    ) {
+        return "Game Tied";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") || 
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper") 
+    ){
+        humanScore++;
         return "You Won! " + humanChoice + " beats " + computerChoice;
     }else {
+        computerScore++;
         return "You Lose! " + computerChoice + " beats " + humanChoice;
     }
 }
-
+/*
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-// console.log(computerSelection);
-console.log(humanSelection);
-
 console.log(playRound(humanSelection, computerSelection));
+console.log(`Score log => YOU: ${humanScore} | Computer: ${computerScore}`);
+*/
 
 
+// Step 6: Write the logic to play the entire game
+function playGame(){
+    for(let i=1; i<=5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
 
+        console.log(`Round: ${i}`)
+        console.log(`computerChoice: ${computerSelection} | humanChoice: ${humanSelection}`);
+        console.log(playRound(humanSelection, computerSelection));
+        console.log(`Score log => YOU: ${humanScore} | Computer: ${computerScore}`);
+        console.log("--------------------------------------------");
 
-// ✅ How it works:
-// 	•	Each player secretly chooses one of three options:
-// 	•	Rock ✊
-// 	•	Paper ✋
-// 	•	Scissors ✌️
-// 	•	Both players reveal their choice at the same time.
+    }
+    if(humanScore > computerScore){
+        console.log("🎉 You won the game!"); 
+    }else if(humanScore < computerScore){
+        console.log("🎉 You lose the game!");
+    }else {
+        console.log("🎉 game tied!");
+    }
+}
+playGame();
 
-// ✅ Rules (who wins):
-// 	•	Rock beats Scissors (rock crushes scissors).
-// 	•	Scissors beats Paper (scissors cut paper).
-// 	•	Paper beats Rock (paper covers rock).
-// 	•	If both choose the same, it’s a tie.
+/*
+✅ How it works:
+	•	Each player secretly chooses one of three options:
+	•	Rock ✊
+	•	Paper ✋
+	•	Scissors ✌️
+	•	Both players reveal their choice at the same time.
 
-// Example outcomes:
-// 	•	Player A: Rock ✊, Player B: Scissors ✌️ → Rock wins.
-// 	•	Player A: Paper ✋, Player B: Rock ✊ → Paper wins.
-// 	•	Player A: Scissors ✌️, Player B: Scissors ✌️ → Tie.
+✅ Rules (who wins):
+	•	Rock beats Scissors (rock crushes scissors).
+	•	Scissors beats Paper (scissors cut paper).
+	•	Paper beats Rock (paper covers rock).
+	•	If both choose the same, it’s a tie.
 
-//     // 
+Example outcomes:
+	•	Player A: Rock ✊, Player B: Scissors ✌️ → Rock wins.
+	•	Player A: Paper ✋, Player B: Rock ✊ → Paper wins.
+	•	Player A: Scissors ✌️, Player B: Scissors ✌️ → Tie.
+*/
