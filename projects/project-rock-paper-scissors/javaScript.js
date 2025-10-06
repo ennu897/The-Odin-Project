a53f11080c73a5d832a3f341f8cdb1d9a1ab7a1b
Anosh Kudikala <102867566+ennu897@users.noreply.github.com>
2025-10-06 14:37:49 -0500

    ui-rps

--- projects/project-rock-paper-scissors/javaScript.js
+++ projects/project-rock-paper-scissors/javaScript.js
@@ -1,10 +1,289 @@
 // https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
 
-const result = document.getElementById('my-heading');
 
-function display(message){
-    result.innerHTML += "<br>" + message;
+function getComputerChoice(){
+    let random = Math.random();
+    if(random < 0.33){
+        return "rock";
+    }else if(random < 0.66){
+        return "paper";
+    }else {
+        return "scissors";
+    }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getHumanChoice(){
+    let userInput = prompt(`input rock, paper or scissors in the below field`);
+    return userInput.toLowerCase();
+}
+
+// let humanSelection = getHumanChoice();
+// let computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice == computerChoice){
+        return "Game Tied!!"; 
+    }else if(
+        (humanChoice == 'rock' && computerChoice == 'scissors') || 
+        (humanChoice == 'scissors' && computerChoice == 'paper') ||
+        (humanChoice == 'paper' && computerChoice == 'rock')
+    ) {
+        humanScore++;
+        // return ` You Won!! ${humanChoice} beats ${computerChoice}`;
+        return ` You Won!! ${humanChoice} beats ${computerChoice}`;
+    }else{
+        computerScore++;
+        // return `You Lost ${computerChoice} beats ${humanChoice}`;
+        return `You Lost ${computerChoice} beats ${humanChoice}`;
+    }
+}
+
+function playGame(humanSelection){
+    
+    let computerSelection = getComputerChoice();
+    
+    display(`HumanSelection: ${humanSelection} || ComputerSelction= ${computerSelection}`);
+    display(playRound(humanSelection, computerSelection)); 
+    display(`humanScore: ${humanScore} || computerScore: ${computerScore}`);
+    
+    if(humanScore == 5 || computerScore == 5){
+        if(humanScore > computerScore){
+            display(`You Won at the END!`);
+        }else if(humanScore < computerScore){
+            display(`You Lost to the Computer at the END`);
+        }else {
+            display(`Game Tied!`);
+        }
+        disableButtons();
+        showResartBtn();
+
+    }
+}
+// playGame();
+
+const div = document.querySelector('div');
+const btn1 = document.createElement('button');
+const btn2 = document.createElement('button');
+const btn3 = document.createElement('button');
+const p = document.createElement('p');
+let restart;
+
+div.appendChild(btn1);
+btn1.textContent = 'rock';
+
+btn1.addEventListener('click', () => {
+    return playGame('rock');
+});
+
+div.appendChild(btn2);
+btn2.textContent = 'scissors';
+
+btn2.addEventListener('click', () => {
+    return playGame('scissors');
+});
+
+div.appendChild(btn3);
+btn3.textContent = 'paper';
+
+btn3.addEventListener('click', () => {
+    return playGame('paper');
+});
+
+div.appendChild(p);
+function display(message){
+    p.innerHTML += message + "<br>"
+}
+
+function disableButtons(){
+    btn1.disabled = true;
+    btn2.disabled = true;
+    btn3.disabled = true;
+}
+
+function restartBtn(){
+    humanScore = 0;
+    computerScore = 0;
+    p.innerHTML = "";
+    btn1.disabled = false;
+    btn2.disabled = false;
+    btn3.disabled = false;
+    restart.remove();
+}
+
+function showResartBtn(){
+    restart = document.createElement('button');
+    restart.innerHTML = "Restart Game";
+    div.appendChild(restart);
+    restart.addEventListener('click', restartBtn);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 // Step 1: Setup the project structure
 // console.log("Hello World!");
@@ -25,14 +304,17 @@ function getComputerChoice(){
 
 
 // Step 3: Write the logic to get the human choice
+/*
 function getHumanChoice(){
     let userInput= prompt("Input either one of the following rock/paper/scissors : ");
     return userInput.toLowerCase();
 }
 // console.log(getHumanChoice());
+*/
 
 
 // Step 4: Declare the players score variables
+/*
 let humanScore = 0;
 let computerScore = 0;
 
@@ -63,27 +345,94 @@ console.log(`Score log => YOU: ${humanScore} | Computer: ${computerScore}`);
 
 
 // Step 6: Write the logic to play the entire game
-function playGame(){
-    for(let i=1; i<=5; i++){
-        const humanSelection = getHumanChoice();
+/*
+function playGame(humanSelection){
         const computerSelection = getComputerChoice();
 
-        display(`Round: ${i}`);
         display(`computerChoice: ${computerSelection} | humanChoice: ${humanSelection}`);
         display(playRound(humanSelection, computerSelection));
         display(`Score log => You: ${humanScore} | Computer: ${computerScore}`);
         display("--------------------------------------------");
 
+        if(humanScore == 5 || computerScore == 5){
+            if(humanScore > computerScore){
+                display("🎉 You won the game!"); 
+            }else if(humanScore < computerScore){
+                display("🎉 You lose the game!");
+            }else {
+                display("🎉 game tied!");
+            }
+            disableButtons();
+            showRestartBtn();
+        }
     }
-    if(humanScore > computerScore){
-        display("🎉 You won the game!"); 
-    }else if(humanScore < computerScore){
-        display("🎉 You lose the game!");
-    }else {
-        display("🎉 game tied!");
-    }
+  
+
+// playGame();
+
+const div = document.querySelector('div');
+const btn1 = document.createElement('button');
+const btn2 = document.createElement('button');
+const btn3 = document.createElement('button');
+const result = document.createElement('p');
+let restartBtn;
+
+
+
+function display(message){
+    result.innerHTML += message + "<br>";
 }
-playGame();
+
+function restartButton(){ 
+    humanScore = 0;
+    computerScore = 0;
+    result.innerHTML = "";
+    btn1.disabled  = false;
+    btn2.disabled  = false;
+    btn3.disabled  = false;
+    restartBtn.remove();
+
+    
+}
+
+function showRestartBtn(){
+    restartBtn = document.createElement('button');
+    restartBtn.textContent = "🔄 Restart Game";
+    div.appendChild(restartBtn);
+    restartBtn.addEventListener('click', restartButton);
+   
+}
+
+function disableButtons(){
+    btn1.disabled  = true;
+    btn2.disabled  = true;
+    btn3.disabled  = true;
+
+}
+
+div.appendChild(btn1);
+btn1.textContent = "rock";
+btn1.addEventListener('click', () => {
+    return playGame(btn1.textContent);
+
+});
+
+div.appendChild(btn2);
+btn2.textContent = "scissors";
+btn2.addEventListener('click', () => {
+    return playGame(btn2.textContent);
+
+});
+
+div.appendChild(btn3);
+btn3.textContent = "paper";
+btn3.addEventListener('click', () => {
+    return playGame(btn3.textContent);
+});
+
+div.appendChild(result)
+
+*/
 
 /*
 ✅ How it works:
