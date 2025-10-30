a2563b2d6549d97ac9850af3f6e55bca14e57301
Anosh Kudikala <102867566+ennu897@users.noreply.github.com>
2025-10-30 12:13:49 -0500

    2nd draft tic-tac-toe

--- projects/javascript-projects/tic-tac-toe/js.js
+++ projects/javascript-projects/tic-tac-toe/js.js
@@ -1,12 +1,33 @@
-document.querySelector('#one-1').addEventListener('click', (e) => {
-  console.log(e);
-})
 
+
+
+let clickCount = 0;
+const restartMsg = document.querySelector('#restart-msg')
+const boxes = document.querySelectorAll('.boxes');
 
 document.querySelector('.grid-three-col').addEventListener('click', (e) => {
+  
   if(e.target.classList.contains('boxes')){
-    let random = Math.random();
-    const message = random <= 0.5 ? 'X' : "O";
-    e.target.value = message;
+    if(e.target.value) return;
+    if(clickCount >= 9) return;
+    
+    clickCount++;
+    e.target.value = clickCount % 2 === 0 ? 'O' : 'X';
+    console.log(clickCount);
+    if(clickCount === 9) {
+      restartMsg.innerHTML = 'You must Restart or Start the game to play again.';
+    }
   }
+});
+
+document.querySelectorAll('.reset-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    clickCount=0;
+    boxes.forEach(box => {
+      box.value='';
+      box.disabled = false;
+    });
+    restartMsg.innerHTML = '';
+    console.log(clickCount);
+  });
 });
