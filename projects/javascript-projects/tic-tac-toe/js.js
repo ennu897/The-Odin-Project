bdb69fac4d2806e409db22d949901bd8b8b6c4b2
Anosh Kudikala <102867566+ennu897@users.noreply.github.com>
2025-10-30 10:32:51 -0500

    intital draft on tit-tac-toe

--- projects/javascript-projects/tic-tac-toe/js.js
+++ projects/javascript-projects/tic-tac-toe/js.js
@@ -1,1 +1,12 @@
-let 
+document.querySelector('#one-1').addEventListener('click', (e) => {
+  console.log(e);
+})
+
+
+document.querySelector('.grid-three-col').addEventListener('click', (e) => {
+  if(e.target.classList.contains('boxes')){
+    let random = Math.random();
+    const message = random <= 0.5 ? 'X' : "O";
+    e.target.value = message;
+  }
+});
