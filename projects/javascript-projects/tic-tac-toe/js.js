d63fd3d399e19f810c63fb9c5aec0162767e971f
Anosh Kudikala <102867566+ennu897@users.noreply.github.com>
2025-10-31 15:37:23 -0500

    draft v wout winner

--- projects/javascript-projects/tic-tac-toe/js.js
+++ projects/javascript-projects/tic-tac-toe/js.js
@@ -1,33 +1,46 @@
-
-
-
 let clickCount = 0;
+const playerOne = document.querySelector("#player-one");
+const playerTwo = document.querySelector("#player-two");
+const playerAssignment = document.querySelector('#player-assignment');
+const start = document.querySelector('#start');
+const restart = document.querySelector('#restart');
 const restartMsg = document.querySelector('#restart-msg')
 const boxes = document.querySelectorAll('.boxes');
 
-document.querySelector('.grid-three-col').addEventListener('click', (e) => {
-  
-  if(e.target.classList.contains('boxes')){
-    if(e.target.value) return;
-    if(clickCount >= 9) return;
+playerAssignment.innerHTML = 'Enter Player names & click Start to begin!';
+
+start.addEventListener('click', () => {
+  if(playerOne.value && playerTwo.value){
+    playerAssignment.innerHTML = `${playerOne.value} is assigned:X ${playerTwo.value} is assigned:O`;
+
+    document.querySelector('.grid-three-col').addEventListener('click', (e) => {
     
-    clickCount++;
-    e.target.value = clickCount % 2 === 0 ? 'O' : 'X';
-    console.log(clickCount);
-    if(clickCount === 9) {
-      restartMsg.innerHTML = 'You must Restart or Start the game to play again.';
-    }
+      if(e.target.classList.contains('boxes')){  
+        if(e.target.value) return;
+        if(clickCount >= 9) return;
+      
+        clickCount++;
+        e.target.value = clickCount % 2 === 0 ? 'O' : 'X';
+        if(clickCount === 9) {
+          restartMsg.style.backgroundColor= 'white';
+          restartMsg.innerHTML = 'You must Restart the game to play again.';
+        }
+      }
+    });
+  }else if(playerOne.value || playerTwo.value){
+    playerAssignment.innerHTML = 'Please enter both player names to Begin!';
   }
 });
 
-document.querySelectorAll('.reset-btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    clickCount=0;
-    boxes.forEach(box => {
-      box.value='';
-      box.disabled = false;
-    });
-    restartMsg.innerHTML = '';
-    console.log(clickCount);
+
+restart.addEventListener('click', () => {
+  clickCount=0;
+  boxes.forEach(box => {
+    box.value='';
+    box.disabled = false;
   });
+  restartMsg.innerHTML = '';
+  playerOne.value = '';
+  playerTwo.value = '';
+  playerAssignment.innerHTML = '';
 });
